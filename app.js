7d3eba5493d108c859b271b5846fb5837a185f8e
Lukemciel <lucasmaciel7ynine@gmail.com>
2023-12-14 22:56:42 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('./public'));
+
+app.listen(8000, ()=>{
+    console.log('servidor corriendo');
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
